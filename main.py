246cae5b49d79a7826bb808df678ec4069e11e8a
Aayush Sharma <bhaskarsharma64047@gmail.com>
2023-10-06 15:48:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -70,7 +70,7 @@ def loopthread(message):
 # start command
 @app.on_message(filters.command(["start"]))
 def send_start(client: pyrogram.client.Client, message: pyrogram.types.messages_and_media.message.Message):
-    app.send_message(message.chat.id, f"__👋 Hi **{message.from_user.mention}**, i am Link Bypasser Bot, just send me any supported links and i will you get you results.\nCheckout /help to Read More__",
+    app.send_message(message.chat.id, f"__👋 Hii **{message.from_user.mention}**, i am Link Bypasser Bot, just send me any supported links and i will you get you results.\nCheckout /help to Read More__",
     reply_markup=InlineKeyboardMarkup([[ InlineKeyboardButton("Updates", url="https://t.me/z_harbour")]]), reply_to_message_id=message.id)
 
 
